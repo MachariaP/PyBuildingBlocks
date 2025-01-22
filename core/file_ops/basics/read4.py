2274722baac3978893cc13766af5462a849398c5
Phinehas <walburphinehas78@gmail.com>
2025-01-22 12:45:46 +0300

    Added Docstring

--- core/file_ops/basics/read4.py
+++ core/file_ops/basics/read4.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 """
+Demonstrates safe file reading with error handling.
 """
 
 def safe_read_file(filename):
+    """
+    Safely read and print contents of a file with error handling.
 
+    Parameters:
+        filename: str: Name of the file to read.
+
+    Returns:
+        None
+    """
     try:
 
         with open(filename, 'r') as file:
